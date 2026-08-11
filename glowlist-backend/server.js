b5b0ea5d59d965d72ae34f240ccacc6a86978d0f
ponorogomela-blip <ponorogomela@gmail.com>
2026-08-11 11:41:53 +0700

    menambahkan produk dan kategori

--- glowlist-backend/server.js
+++ glowlist-backend/server.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'glowlist_db'
+});
+db.connect(err => {
+    if (err) {
+        console.error('Gagal konek ke database:', err);
+    } else {
+        console.log('Berhasil konek ke database GlowList')
+    }
+});
 const PORT = 3001;
 
 app.use(express.json());
-
 app.get('/', (req, res) => {
-    res.send('Selamat datang di GlowList API! ');
+    res.send('Selamat Datang di GlowList API!!!!! ');
 });
-
+app.get('/produk', (req, res) => {
+    const sql = 'SELECT * FROM produk';
+    db.query(sql, (err, results) => {
+        if(err) return resstatus(500).json({ error: err });
+        res.json(results);
+    });
+});
 app.listen(PORT, () => {
     console.log(`Server GlowList jalan di http://localhost:${PORT}`);
+});
+app.get('/kategori', (req, res) => {
+    const sql = 'SELECT * FROM kategori';
+    db.query(sql, (err, results) => {
+        if(err) return resstatus(500).json({ error: err });
+        res.json(results);
+    });
 });
