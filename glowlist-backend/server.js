d65fd1c71c393ec9dac3ed7b6dfda3b004b02024
ponorogomela-blip <ponorogomela@gmail.com>
2026-08-12 09:41:17 +0700

    tentang saya

--- glowlist-backend/server.js
+++ glowlist-backend/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mysql = require('mysql2');
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'glowlist_db'
+    database: 'glowlist_db_melaila'
 });
 db.connect(err => {
     if (err) {
@@ -15,26 +20,27 @@ db.connect(err => {
         console.log('Berhasil konek ke database GlowList')
     }
 });
-const PORT = 3001;
 
-app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server GlowList jalan di http://localhost:${PORT}`);
+});
+
 app.get('/', (req, res) => {
     res.send('Selamat Datang di GlowList API!!!!! ');
 });
 app.get('/produk', (req, res) => {
     const sql = 'SELECT * FROM produk';
     db.query(sql, (err, results) => {
-        if(err) return resstatus(500).json({ error: err });
+        if(err) return res.status(500).json({ error: err });
         res.json(results);
     });
 });
-app.listen(PORT, () => {
-    console.log(`Server GlowList jalan di http://localhost:${PORT}`);
-});
+
 app.get('/kategori', (req, res) => {
     const sql = 'SELECT * FROM kategori';
     db.query(sql, (err, results) => {
-        if(err) return resstatus(500).json({ error: err });
+        if(err) return res.status(500).json({ error: err });
         res.json(results);
     });
 });
---
+++ glowlist-frontend/src/pages/Tentang.jsx
@@ -0,0 +1,8 @@
+export default function tentang() {
+    return <p>Nama : Melaila Putri Nanda Sabrina
+        Kelas : XII RPL 2
+        Saya suka sekali dengan skincare karena rutinitas ini memberikan waktu bagi diri sendiri (me time) untuk bersantai,
+        merawat kesehatan kulit agar tetap bersih dan lembap, 
+        serta meningkatkan rasa percaya diri melihat perubahan positif pada tekstur wajah yang jadi lebih glowing dan segar setiap hari😍✨😽
+    </p>;
+}
