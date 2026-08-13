8e78945bec652b3d38fb24a58afbc15a75b20478
ponorogomela-blip <ponorogomela@gmail.com>
2026-08-13 12:09:28 +0700

    menambahkan Response 404 pada Route PUT & DELETE

--- glowlist-backend/server.js
+++ glowlist-backend/server.js
@@ -50,6 +50,41 @@ app.post('/produk',(req, res) =>  {
     });
 });
 
+app.put('/produk/:id_produk', (req, res) => {
+    const { id_produk } = req.params;
+    const { judul, deskripsi, harga, id_kategori } = req.body;
+
+    if (!judul || !harga) {
+        return res.status(400).json({ message: 'judul, dan harga wajib diisi'});
+    }
+    //console.log("dfasd");
+    
+    const sql= 'UPDATE produk SET judul=?, deskripsi=?, harga=?, id_kategori=? WHERE id_produk=?';
+    db.query(sql, [judul, deskripsi, harga, id_kategori, id_produk], (err, result) => {
+        if (err) return res.status(500).json({ error: err.sqlMessage });
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Produk tidak ditemukan' });
+        }
+
+        res.json({ message: 'Produk berhasil diupdate' });
+    });
+});
+
+app.delete('/produk/:id_produk', (req, res) => {
+    const { id_produk } = req.params;
+    const sql = 'DELETE FROM produk WHERE id_produk = ?';
+    db.query(sql, [id_produk], (err, result) => {
+        if (err) return res.status(500).json({ error: err.sqlMessage });
+
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ message: 'Produk tidak ditemukan' });
+        }
+
+        res.json({ message: 'Produk berhasil dihapus!' });
+    });
+});
+
 app.get('/kategori', (req, res) => {
     const sql = 'SELECT * FROM kategori';
     db.query(sql, (err, results) => {
